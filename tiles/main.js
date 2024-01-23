f14e66799acdc255cbb8ec9f6a731f46309cc045
quantenschaum <woxpox@posteo.de>
2024-01-23 22:12:04 +0100

    fixed qgis server and docker setup

--- tiles/main.js
+++ tiles/main.js
@@ -206,6 +206,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if(isLocal) {
     overlays['QGIS BSH'] = L.tileLayer('http://localhost:8001/tiles/bsh/EPSG3857/{z}/{x}/{y}.png');
+    overlays['QGIS BSH WMS'] = L.tileLayer.wms('http://localhost:8000/', {
+            layers:'bsh',
+            transparent:true,
+            format:'image/png',
+            version:'1.3.0'});
   }
 
   var map = L.map('map', {
