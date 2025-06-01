27d127cae4ce3e64ac466820cb2deebe1bd26a26
quantenschaum <woxpox@posteo.de>
2025-06-01 09:35:35 +0200

    updated vector chart

--- www/vector.js
+++ www/vector.js
@@ -1,39 +1,37 @@
 var style=(new URL(document.location)).searchParams.get("style");
 if(!style) style='style.json';
 
+async function mergeStyles(styleUrl1, styleUrl2) {
+  const [style1, style2] = await Promise.all([
+    fetch(styleUrl1).then(res => res.json()),
+    fetch(styleUrl2).then(res => res.json())
+  ]);
 
-var map = new maplibregl.Map({
-    container: 'map', // container id
-//    style: 'https://demotiles.maplibre.org/style.json',
-//    style: 'https://api.maptiler.com/maps/openstreetmap/style.json?key=L8FrrrJGE2n415wJo8BL',
-    style: style,
-    center: [9.239,54.397], // starting position [lng, lat]
-    zoom: 7, // starting zoom
-    hash: true,
-});
+  const mergedSources = {
+    ...style1.sources,
+    ...style2.sources
+  };
 
-map.addControl(new maplibregl.NavigationControl(),'top-left');
+  const mergedLayers = [...style1.layers, ...style2.layers];
 
-map.on('click', 'SkinOfTheEarth', (e) => {
-  console.log(e);
-  new maplibregl.Popup()
-    .setLngLat(e.lngLat)
-    .setHTML(`${e.features[0].properties}`)
-    .addTo(map);
-});
+  const mergedStyle = {
+    ...style1,
+    sprite: style2.sprite,
+    sources: mergedSources,
+    layers: mergedLayers
+  };
 
-//map.on("styleimagemissing", e=>{
-//  var id=e.id;
-//  var size=64;
-//  var url=(id.includes('INT1')?id:"icons/"+id)+".svg";
-//  var img=new Image(size,size);
-////  console.log("svg",id,url);
-//  img.onload = ()=>{
-//    console.log("loaded",id,url);
-//    if(!map.hasImage(id)) {
-//      map.addImage(id, img);
-//    }
-//  };
-//  img.src=url;
-//});
+  return mergedStyle;
+}
 
+mergeStyles('https://api.maptiler.com/maps/openstreetmap/style.json?key=L8FrrrJGE2n415wJo8BL', style)
+  .then(mergedStyle => {
+    const map = new maplibregl.Map({
+      container: 'map',
+      style: mergedStyle,
+      center: [9.239,54.397], // starting position [lng, lat]
+      zoom: 7, // starting zoom
+      hash: true,
+    });
+    map.addControl(new maplibregl.NavigationControl(),'top-left');
+  });
