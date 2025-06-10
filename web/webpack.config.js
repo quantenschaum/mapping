7ba25805e98e76600a1b1e8a6dd5fc5d0bae05a4
quantenschaum <woxpox@posteo.de>
2025-06-10 20:34:49 +0200

    update service worker caching rules to include CSS files and rename cache key to 'assets'

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -44,10 +44,10 @@ module.exports = (env, argv) => {
         ignoreURLParametersMatching: [/.*/],
         runtimeCaching: [
           {
-            urlPattern: /\/download\/.*(html|js|xml|webp|png|jpe?g|json|\/)$/,
+            urlPattern: /\/download\/.*\.(html|js|xml|webp|png|jpe?g|json|css|\/)$/,
             handler: 'NetworkFirst',
             options: {
-              cacheName: 'other',
+              cacheName: 'assets',
               networkTimeoutSeconds: 3,
               expiration: {
                 maxEntries: 100,
