d0ea1b1682b1ae214bc12cccc005382bf84026eb
quantenschaum <woxpox@posteo.de>
2025-12-20 19:33:45 +0100

    restore layer opacity

--- web/src/restore.js
+++ web/src/restore.js
@@ -1,29 +1,31 @@
-import 'leaflet-hash';
-import {log, debounce} from './utils';
+import "leaflet-hash";
+import { log, debounce } from "./utils";
 
-const isStandalone = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone;
+const isStandalone =
+  window.matchMedia("(display-mode: standalone)").matches ||
+  window.navigator.standalone;
 
 function logStore(...args) {
-  log('Store', 'blue', ...args);
+  log("Store", "blue", ...args);
 }
 
 function logRestore(...args) {
-  log('Restore', 'blue', ...args);
+  log("Restore", "blue", ...args);
 }
 
 export function restoreLayers(control, list) {
-
   const map = control._map;
   const storage = isStandalone ? localStorage : sessionStorage;
 
-  const LAYERS = 'layers';
+  const LAYERS = "layers";
 
   function storeActiveLayers() {
-    let active = [];
+    const active = {};
     for (let i = 0; i < control._layers.length; i++) {
       let l = control._layers[i];
       if (map.hasLayer(l.layer)) {
-        active.push(l.name);
+        // active.push(l.name);
+        active[l.name] = l.layer?.options?.opacity ?? 1;
       }
     }
     logStore("layers", active);
@@ -31,31 +33,44 @@ export function restoreLayers(control, list) {
   }
 
   function restoreActiveLayers() {
-    let active = storage.getItem(LAYERS);
-    active = JSON.parse(active);
-    if (list) active = list.split(',');
-    if (!active) return;
-    logRestore("layers", active);
-    for (let i = 0; i < control._layers.length; i++) {
-      let l = control._layers[i];
-      if (active.includes(l.name)) {
-        map.addLayer(l.layer);
-      } else {
-        map.removeLayer(l.layer);
+    try {
+      let active = JSON.parse(storage.getItem(LAYERS));
+      if (list) {
+        active = {};
+        list.split(",").forEach((n) => (active[n] = 1));
       }
+      if (!active) return;
+      logRestore("layers", active);
+      for (let i = 0; i < control._layers.length; i++) {
+        let l = control._layers[i];
+        let o = active[l.name];
+        if (o != null) {
+          l.layer.options.opacity = o;
+          map.addLayer(l.layer);
+        } else {
+          map.removeLayer(l.layer);
+        }
+      }
+    } catch (e) {
+      console.error(e);
     }
   }
 
-
   restoreActiveLayers();
-  map.on('layeradd layerremove overlayadd overlayremove', debounce(storeActiveLayers));
 
+  map.on(
+    "layeradd layerremove overlayadd overlayremove",
+    debounce(storeActiveLayers),
+  );
 
-  const VIEW = 'view';
+  const VIEW = "view";
 
   function storeView() {
     logStore("view", map.getZoom(), map.getCenter());
-    storage.setItem(VIEW, JSON.stringify({center: map.getCenter(), zoom: map.getZoom()}));
+    storage.setItem(
+      VIEW,
+      JSON.stringify({ center: map.getCenter(), zoom: map.getZoom() }),
+    );
   }
 
   function restoreView() {
@@ -67,9 +82,9 @@ export function restoreLayers(control, list) {
 
   if (isStandalone) {
     restoreView();
-    map.on('moveend zoomend', debounce(storeView));
+    map.on("moveend zoomend", debounce(storeView));
   } else {
-    logRestore('use hash');
+    logRestore("use hash");
     new L.Hash(map);
   }
 }
