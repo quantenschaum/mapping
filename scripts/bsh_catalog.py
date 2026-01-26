b7541bfcd468a23b7bfb1431fe11265c91482bd9
quantenschaum <woxpox@posteo.de>
2026-01-26 09:23:30 +0100

    Refactor bsh_catalog.py for improved readability and add JSON export

--- scripts/bsh_catalog.py
+++ scripts/bsh_catalog.py
@@ -2,22 +2,30 @@
 # coding: utf-8
 
 import os.path
-from pyquery import PyQuery as pq
 
 from filter import save_json
+from pyquery import PyQuery as pq
+
 
 def main():
-  xml=pq(filename=os.path.dirname(__file__)+'/../data/bsh/catalog.html')
-  # print(xml)
-  catalog={}
-  for enc in xml('enc_entry'):
-    enc=pq(enc)
-    # print(enc)
-    meta={c.tag:c.text for c in enc('*') if 'gml' not in c.tag and c.text and c.text.strip()}
-    print(meta['c_number'])
-    catalog[meta['c_number']]=meta
+    xml = pq(filename=os.path.dirname(__file__) + "/../data/bsh/catalog.html")
+    # print(xml)
+    catalog = {}
+    for enc in xml("enc_entry"):
+        enc = pq(enc)
+        # print(enc)
+        meta = {
+            c.tag: c.text
+            for c in enc("*")
+            if "gml" not in c.tag and c.text and c.text.strip()
+        }
+        print(meta["c_number"])
+        catalog[meta["c_number"]] = meta
 
-  save_json(os.path.dirname(__file__)+'/../data/bsh/catalog.json',catalog,indent=2)
+    save_json(
+        os.path.dirname(__file__) + "/../data/bsh/catalog.json", catalog, indent=2
+    )
 
-if __name__=='__main__':
-  main()
+
+if __name__ == "__main__":
+    main()
