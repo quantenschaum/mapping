31b2f32d0880f8f5bbe02bb8b853a0bd4bb0c5b9
quantenschaum <woxpox@posteo.de>
2026-02-14 23:17:11 +0100

    compact print buttons

--- web/src/print.js
+++ web/src/print.js
@@ -12,6 +12,48 @@ export const PrintButton = L.Control.extend({
     );
     L.DomEvent.disableClickPropagation(div);
 
+    async function getImage() {
+      const size = map.getSize();
+      const body = document.body;
+      body.classList.add("imagemode");
+      await domtoimage
+        .toPng(map.getContainer(), {
+          height: size.y,
+          width: size.x,
+          // filter: function (node) {
+          //   if (node.tagName === "IMG") {
+          //     const img = node;
+          //     if (img.complete && img.naturalWidth === 0) {
+          //       console.log("Excluding failed/CORS image:", img.src);
+          //       return false;
+          //     }
+          //     const isSameOrigin =
+          //       img.src.startsWith(window.location.origin) ||
+          //       img.src.startsWith("data:") ||
+          //       img.src.startsWith("blob:");
+          //     if (!isSameOrigin && !img.crossOrigin) {
+          //       console.log("Excluding external image without CORS:", img.src);
+          //       return false;
+          //     }
+          //   }
+          //   return true;
+          // },
+        })
+        // .catch(console.error)
+        .then(function (dataUrl) {
+          const link = L.DomUtil.create("a");
+          link.download = `freenauticalchart-${map.getZoom()}-${map.getCenter().lat.toFixed(3)}-${map.getCenter().lng.toFixed(3)}.png`;
+          link.href = dataUrl;
+          link.click();
+          ackee.action("4e923284-945c-45fd-97d6-4c0d351205ec", {
+            key: "print_export",
+            value: 1,
+          });
+        })
+        .catch(console.error);
+      body.classList.remove("imagemode");
+    }
+
     const classes = [];
 
     function button(name, title, cls = "") {
@@ -30,8 +72,14 @@ export const PrintButton = L.Control.extend({
           });
         }
         const cl = cont.classList;
-        if (cls) {
+        const isSelected = clss.every((c) => cl.contains(c));
+        if (!cls) {
+          getImage();
+        } else if (!isSelected) {
           body.classList.add("print");
+          const formats = pb.querySelectorAll(".format");
+          formats.forEach((f) => f.classList.remove("selected"));
+          b.classList.add("selected");
           classes.forEach((c) => cl.remove(c));
           clss.forEach((c) => cl.add(c));
           map.invalidateSize();
@@ -40,6 +88,7 @@ export const PrintButton = L.Control.extend({
           map.options.wheelPxPerZoomLevel = 100;
         } else {
           body.classList.remove("print");
+          b.classList.remove("selected");
           classes.forEach((c) => cl.remove(c));
           map.invalidateSize();
           delete map.options.zoomDelta;
@@ -51,44 +100,15 @@ export const PrintButton = L.Control.extend({
       return b;
     }
 
-    const pb = L.DomUtil.create("div", "printbuttons hidebuttons");
+    const pb = L.DomUtil.create("div", "printbuttons hide");
     div.appendChild(pb);
-    pb.appendChild(button("&nbsp;", "reset print layout"));
+    pb.appendChild(button("&nbsp;", "print/download image"));
     pb.appendChild(button("A4", "A4 landscape", "A4 landscape"));
     pb.appendChild(button("A4", "A4 portrait", "A4 portrait"));
     pb.appendChild(button("A3", "A3 landscape", "A3 landscape"));
     pb.appendChild(button("A3", "A3 portrait", "A3 portrait"));
-    pb.addEventListener("mouseover", () => {
-      pb.classList.remove("hidebuttons");
-    });
-    pb.addEventListener("mouseout", () => {
-      pb.classList.add("hidebuttons");
-    });
-
-    const imgButton = L.DomUtil.create("a", "imagebutton");
-    imgButton.innerHTML = "&nbsp;";
-    imgButton.title = "download image";
-    div.appendChild(imgButton);
-    imgButton.addEventListener("click", async () => {
-      const size = map.getSize();
-      const body = document.body;
-      body.classList.add("image");
-      // await new Promise(r => setTimeout(r, 500));
-      await domtoimage
-        .toPng(map.getContainer(), { height: size.y, width: size.x })
-        .then(function (dataUrl) {
-          const link = L.DomUtil.create("a");
-          link.download = `freenauticalchart-${map.getZoom()}-${map.getCenter().lat.toFixed(3)}-${map.getCenter().lng.toFixed(3)}.png`;
-          link.href = dataUrl;
-          link.click();
-          ackee.action("4e923284-945c-45fd-97d6-4c0d351205ec", {
-            key: "print_export",
-            value: 1,
-          });
-        })
-        .catch(console.error);
-      body.classList.remove("image");
-    });
+    pb.addEventListener("mouseenter", () => pb.classList.remove("hide"));
+    pb.addEventListener("mouseleave", () => pb.classList.add("hide"));
 
     return div;
   },
